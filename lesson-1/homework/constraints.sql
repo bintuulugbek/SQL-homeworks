9f826f07ab065d92e9043638d01e21037c9a3aa3
bintuulugbek <163106257+bintuulugbek@users.noreply.github.com>
2025-02-27 18:10:14 +0500

    Update hw.sql

--- lesson-1/homework/constraints.sql
+++ lesson-1/homework/constraints.sql
@@ -1,1 +1,23 @@
+--hw_1
+
+--1.Practical Tasks
+create database HomeworkDB
+
+create table Students (StudentsID int, FullName varchar(50), age int, GPA decimal(3,2))
+
+alter table Students
+add email varchar(50)
+
+sp_rename 'Students.Fullname', 'Name', 'COLUMN'
+
+alter table Students
+drop column age
+
+truncate table Students
+
+--2.Query Writing
+select @@SERVERNAME
+select GETDATE()
+select * from Students
+
 
